159d3db084a543846e7ce460d53574a5434e5b58
MagicHatJo <jnochang@gmail.com>
2019-07-06 18:20:56 -0700

    more stuff

--- mkpro/project_c.py
+++ mkpro/project_c.py
@@ -1,6 +1,17 @@
 
+import os
 
 
+def create_subdirs(args):
+    try:
+        os.mkdir(args.project_name + "/src")
+        os.mkdir(args.project_name + "/inc")
+        os.mkdir(args.project_name + "/lib")
+        os.mkdir(args.project_name + "/doc")
+        os.mkdir(args.project_name + "/test")
+    except:
+        print("Failed to create subdirectories")
 
 def new(args):
     print("Inside C new")
+    create_subdirs(args)
